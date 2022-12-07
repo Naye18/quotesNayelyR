06c98b964cc05af365166aab5f02219699049d64
Naye18 <91495047+Naye18@users.noreply.github.com>
2022-12-07 02:09:36 -0600

    Add files via upload

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_edu_itvo_quotescelebrities_QuotesApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "edu.itvo.quotescelebrities.QuotesApp"
+)
+public final class _edu_itvo_quotescelebrities_QuotesApp {
+}
